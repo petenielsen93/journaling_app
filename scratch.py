c4ab84636261b3b7088e7dc1ea14402e35071798
petenielsen93 <petenielsen93@gmail.com>
2023-09-25 11:41:37 -0400

    Added tkinter GUI that populates calendar. Selecting a date and pressing the button opens a journal entry for each day.

--- scratch.py
+++ scratch.py
@@ -1,12 +1,22 @@
 from datetime import datetime
 import os
+from tkinter import *
+from tkcalendar import *
+import re
 
-def newfile():
-    current_date = datetime.now().strftime('%Y-%m-%d')
+def sanitize_filename(filename):
+    return re.sub(r'[\\/:"*?<>|]+', '', filename)
+
+root=Tk()
+
+def newfile(selected_date):
+    #current_date = datetime.now().strftime('%Y-%m-%d')
+    sanitized_date = sanitize_filename(selected_date)
+
     folder_name = "journal_entries"
-    file_name = os.path.join(folder_name, f"{current_date}.txt")
+    file_name = os.path.join(folder_name, f"{sanitized_date}.txt")
     
-    # Ensure the folder exists, create it if it doesn't
+    # Ensures the folder exists, create it if it doesn't
     if not os.path.exists(folder_name):
         os.makedirs(folder_name)
     
@@ -14,14 +24,31 @@ def newfile():
 
     with open(file_name, "a") as file:
         if not file_exists:
-            file.write("Journal Entry: " + f"{current_date}")
+            file.write("Journal Entry: " + f"{selected_date}")
         
     os.system(f"notepad {file_name}")
 
-if __name__ == "__main__":
-    newfile()  # Call the function to create and save the file in the "journal_entries" folder
+def open_journal_entry():
+    selected_date = mycal.get_date()
+    #formatted_date = selected_date.strftime('%Y-%m-%d')
+    newfile(selected_date)
+
+mycal = Calendar(root, setmode="day")
+mycal.pack(padx=15,pady=15)
+
+open_cal=Button(root, text="Open Journal Entry", command=open_journal_entry)
+open_cal.pack(padx=15)
+
+root.geometry("300x300")
+root.title("Calendar")
+root.configure(bg="lightblue")
+
+mainloop()
 
 
-#with this, I create a text file and save it in the root folder. Next I want to save it in a specific folder as a text file. 
-#after that, I want to add this to the tkinter calendar, get and assign each day to a file, create a button that allows you to
-#open a file for that date/save it/reopen
+#Not sure why it is no longer saving each entry into the journal_entries folder, but we have our MVP. Brainstorm on cosmetic and further function changes. 
+
+#if __name__ == "__main__":
+    #newfile()  # Call the function to create and save the file in the "journal_entries" folder
+
+
